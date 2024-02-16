8450bc7e18d5a2e6718830b61057e831992883f3
nithin <nithineruventi@gmail.com>
2024-02-16 22:35:38 +0530

    delete function add to the blog

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,41 @@
+import { useState , useEffect } from 'react';
+import BlogList from './Bloglist';
+
 const Home = () => {
-    return ( 
+    const [blogs, setBlogs] = useState([
+        {
+            title: "My blog1",
+            body: "First blog in the website",
+            author: "Nithin",
+            id: 1
+        },
+        {
+            title: "My blog2",
+            body: "Second blog in the website",
+            author: "Nithin Kumar",
+            id: 2
+        },
+        {
+            title: "My blog3",
+            body: "Third blog in the website",
+            author: "Eruventi Nithin Kumar",
+            id: 3
+        }
+    ]);
+    const handleDelete=(id)=> {
+       const newBlogs = blogs.filter(blog=> blog.id !== id);
+       setBlogs(newBlogs);
+        
+    };
+    useEffect(()=>{
+        console.log('use effect ran');
+    })
+    return (
         <div className="home">
             <h2>Homepage</h2>
+            <BlogList blogs={blogs}  handleDelete={handleDelete} />
         </div>
-     );
+    );
 }
- 
+
 export default Home;
